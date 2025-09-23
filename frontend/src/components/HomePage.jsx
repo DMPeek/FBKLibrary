c95330bb889eb5f2f4325714784b4db65163317f
DMPeek <dustinmpeek@outlook.com>
2025-09-22 20:46:31 -0500

    updating logo

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -193,7 +193,8 @@ export default function HomePage() {
     <div>
       <header className="header-bar">
         <div className="header-left">
-          <a href="/" className="logo">FBK<span className="ai-logo">AI</span>ZO</a>
+          {/* <a href="/" className="logo">FBK<span className="ai-logo">AI</span>ZO</a> */}
+          <a href="/" className="logo">FBKaizo Labs</a>
         </div>
         <nav className="header-right">
           <a href="/">Homepage</a>
