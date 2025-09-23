1d4c56641edf21e888a4a4a2d214581b38552edc
DMPeek <dustinmpeek@outlook.com>
2025-09-23 12:15:20 -0500

    setting up skeleton

---
+++ frontend/src/components/Orbs.jsx
@@ -0,0 +1,9 @@
+import { useState, useRef, useEffect } from 'react';
+import '../styles.css';
+import { Monsters } from '../assets/monsters';
+
+export default function Orbs() {
+    return (
+        <p>hallo</p>
+    );
+}
